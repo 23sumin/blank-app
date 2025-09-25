e1c46023ff2959a44fc0c7a94f1ae97e95c11d0f
23sumin <sumin051205@naver.com>
2025-09-25 07:04:48 +0000

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,22 @@
+
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("토메함수 개형 시각화")
 
-# 토메함수(예시: tan(x)) 그래프 시각화
-st.header("토메함수 그래프 시각화")
-x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
-y = np.tan(x)
+st.write("토메함수(예: sinc 함수)의 그래프를 아래에서 확인하세요.")
+
+# x 값 범위 설정
+x = np.linspace(-20, 20, 400)
+# sinc 함수 정의 (토메함수)
+y = np.sinc(x / np.pi)
 
 fig, ax = plt.subplots()
-ax.plot(x, y, label='y = tan(x)')
-ax.set_ylim(-10, 10)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_title('토메함수: y = tan(x)')
+ax.plot(x, y)
+ax.set_title("토메함수 y = sinc(x)")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
 ax.grid(True)
-ax.legend()
 
 st.pyplot(fig)
