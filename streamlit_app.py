acdf65bd0f23e6f8c33119b30760d14b4c616c7f
23sumin <sumin051205@naver.com>
2025-09-25 07:01:25 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# 토메함수(예시: tan(x)) 그래프 시각화
+st.header("토메함수 그래프 시각화")
+x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
+y = np.tan(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y, label='y = tan(x)')
+ax.set_ylim(-10, 10)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('토메함수: y = tan(x)')
+ax.grid(True)
+ax.legend()
+
+st.pyplot(fig)
