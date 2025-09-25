5d1615bd63068cf090421da8590558b0d0e3e75e
23sumin <sumin051205@naver.com>
2025-09-25 07:27:53 +0000

    5

--- pages/data.py
+++ pages/data.py
@@ -1,7 +1,16 @@
 
+
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+import os
+
+# NanumGothic 폰트 경로 지정
+font_path = os.path.join(os.path.dirname(__file__), '../fonts/NanumGothic-Regular.ttf')
+font_manager.fontManager.addfont(font_path)
+rc('font', family='NanumGothic')
 
 st.title('간단한 데이터 시각화')
 
@@ -15,8 +24,18 @@ data = pd.DataFrame({
 st.subheader('데이터 미리보기')
 st.dataframe(data)
 
-st.subheader('A 컬럼 히스토그램')
-st.bar_chart(data['A'])
+st.subheader('A 컬럼 히스토그램 (matplotlib)')
+fig, ax = plt.subplots()
+ax.hist(data['A'], bins=20, color='skyblue', edgecolor='black')
+ax.set_title('A 컬럼 히스토그램')
+ax.set_xlabel('값')
+ax.set_ylabel('빈도')
+st.pyplot(fig)
 
-st.subheader('B vs C 산점도')
-st.scatter_chart(data, x='B', y='C')
+st.subheader('B vs C 산점도 (matplotlib)')
+fig2, ax2 = plt.subplots()
+ax2.scatter(data['B'], data['C'], color='tomato')
+ax2.set_title('B vs C 산점도')
+ax2.set_xlabel('B')
+ax2.set_ylabel('C')
+st.pyplot(fig2)
